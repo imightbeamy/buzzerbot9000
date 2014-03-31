a9d4aa4395fbcf56c60c952df5f1872cad221fa7
Amy Ciavolino <amyciav@gmail.com>
2014-03-30 20:49:47 -0400

    Add setup and author to package file, update setup to init DDNS as well

--- setup.sh
+++ setup.sh
@@ -3,13 +3,14 @@ function config {
     node get_config.js $1
 }
 
+DDNS_TIME="1,6,11,16,21,26,31,36,41,46,51,56 * * * * sleep 43"
 DDNS_KEY=$(config freedns_key)
-DDNS_CRON="1,6,11,16,21,26,31,36,41,46,51,56 * * * * sleep 43"
-DDNS_CRON="$DDNS_CRON ; wget -O - http://freedns.afraid.org/dynamic/update.php?$DDNS_KEY"
-DDNS_CRON="$DDNS_CRON >> /tmp/DNS.log 2>&1 &"
+DDNS_CMD="wget -O - http://freedns.afraid.org/dynamic/update.php?$DDNS_KEY"
+DDNS_CRON="$DDNS_TIME ; $DDNS_CMD >> /tmp/DNS.log 2>&1 &"
 
-echo "Adding DDNS cron to crontab:"
-echo "$DDNS_CRON"
+echo "Initing DDNS and adding DDNS cron to crontab:"
+echo "$DDNS_CMD"
+$DDNS_CMD
 crontab -l > temp_cron
 echo "$DDNS_CRON" >> temp_cron
 crontab temp_cron
